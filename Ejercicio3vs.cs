9aacb2d0fd1e9b4e798b43fa0fcdab23323c0091
2017000182-MarcoCastillo <m.castillo.5cdb@gmail.com>
2026-04-16 12:02:44 -0600

    Add files via upload

---
+++ Ejercicio3vs.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Rango de estudiantes");
+        Console.WriteLine("INGRESE UNA LETRA (A, B, C, D, F");
+        char letra = char.ToUpper(Console.ReadKey().KeyChar);
+        Console.WriteLine();
+
+        switch (letra)
+        {
+            case'A':
+                Console.WriteLine("Excelente");
+                break;
+            case 'B':
+                Console.WriteLine("Excelente");
+                break;
+            case 'C':
+                Console.WriteLine("Excelente");
+                break;
+            case 'D':
+                Console.WriteLine("Excelente");
+                break;
+            case 'F':
+                Console.WriteLine("Excelente");
+                break;
+        }
+    }
+}
